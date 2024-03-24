fa1c4488219f0837310fe2ebe083cbaca2c978b8
yigidov <yigitduran19@hotmail.com>
2024-03-24 17:21:24 +0100

    Implemented Task 9

--- pythonProject1/square_generator/square_generator.py
+++ pythonProject1/square_generator/square_generator.py
@@ -14,3 +14,10 @@ class CubicGenerator(SquareGenerator):
             raise ValueError("End of range cannot be less than start")
 
         return [x ** 3 for x in range(start, end + 1)]
+
+    def generate_squares(self, start, end):
+
+        if start > end:
+            raise ValueError("Start of range cannot be greater than end")
+
+        return [x ** 2 for x in range(start, end + 1)]
