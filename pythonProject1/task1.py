f6dc9543e0cf9f4eb6cdca3020398046ab00b1a3
yigidov <yigitduran19@hotmail.com>
2024-03-24 16:06:08 +0100

    Implemented Task 3

--- pythonProject1/task1.py
+++ pythonProject1/task1.py
@@ -16,3 +16,20 @@ if __name__ == "__main__":
 
     print("List of squares of numbers from", start, "to", end, ":")
     print(squares)
+
+
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end+1)]
+
+if __name__ == "__main__":
+    square_gen = SquareGenerator()
+
+    start = 1
+    end = 10
+
+    squares = square_gen.generate_squares(start, end)
+
+    print("List of squares of numbers from", start, "to", end, ":")
+    print(squares)
