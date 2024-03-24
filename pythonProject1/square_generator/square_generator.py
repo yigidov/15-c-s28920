4076acbde8d992a129bb34b2dfdea891f2c0dac8
yigidov <yigitduran19@hotmail.com>
2024-03-24 17:26:29 +0100

    Implemented Task 10

--- pythonProject1/square_generator/square_generator.py
+++ pythonProject1/square_generator/square_generator.py
@@ -1,10 +1,36 @@
 
-class SquareGenerator:
-    def generate_squares(self, start, end):
-        if end < start:
-            raise ValueError("End of range cannot be less than start")
+# class SquareGenerator:
+#     def generate_squares(self, start, end):
+#         if end < start:
+#             raise ValueError("End of range cannot be less than start")
+#
+#         return [x ** 2 for x in range(start, end + 1)]
+#
 
-        return [x ** 2 for x in range(start, end + 1)]
+# class CubicGenerator(SquareGenerator):
+#     def generate_cubes(self, start, end):
+#
+#         if end < start:
+#             raise ValueError("End of range cannot be less than start")
+#
+#         return [x ** 3 for x in range(start, end + 1)]
+#
+#     def generate_squares(self, start, end):
+#
+#         if start > end:
+#             raise ValueError("Start of range cannot be greater than end")
+#
+#         return [x ** 2 for x in range(start, end + 1)]
+#
+#
+
+from abc import ABC, abstractmethod
+
+
+class SquareGenerator(ABC):
+    @abstractmethod
+    def generate_squares(self, start, end):
+        pass
 
 
 class CubicGenerator(SquareGenerator):
