586f20a2825a984a37e23bb8de7e6fe418dce043
yigidov <yigitduran19@hotmail.com>
2024-03-24 16:03:31 +0100

    Implemented Task 2

--- pythonProject1/task1.py
+++ pythonProject1/task1.py
@@ -1,4 +1,18 @@
-squares = [x**2 for x in range(1, 11)]
+squares = [x ** 2 for x in range(1, 11)]
 
 print("List of squares from 1 to 10:")
 print(squares)
+
+
+def e_squares(start, end):
+    return [x ** 2 for x in range(start, end + 1)]
+
+
+if __name__ == "__main__":
+    start = 1
+    end = 10
+
+    squares = e_squares(start, end)
+
+    print("List of squares of numbers from", start, "to", end, ":")
+    print(squares)
